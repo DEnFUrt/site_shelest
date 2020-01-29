fbaee724483ba3a6212b3deef9e37ee2bb76b1e4
DEnFUrt <musorov@gmail.com>
2020-01-29 22:58:02 +0500

    add callback

--- send.php
+++ send.php
@@ -11,7 +11,7 @@ $text = $_POST['question'];
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
     $msg = "ok";
-    
+
     // Настройки SMTP
     $mail->isSMTP();
     $mail->CharSet = "UTF-8";
@@ -19,7 +19,7 @@ try {
     $mail->SMTPDebug = 0;
     $mail->Encoding = 'base64';
     $mail->isHTML(true);
-    
+
     //Обход проверки CA сертификата отключить на реальном хостинге
     $mail->SMTPOptions = array(
         'ssl' => array(
@@ -34,12 +34,12 @@ try {
     $mail->Password   = 'Ljr72vip'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('Shelest.ckcco@gmail.com', 'Обращение с сайта'); // Адрес самой почты и имя отправителя
-    
+    $mail->setFrom('Shelest.ckcco@gmail.com', 'Сообщение с сайта СК ССО Шелест'); // Адрес самой почты и имя отправителя
+
     // Получатель письма
     $mail->addAddress('Nevrolog.pav@yandex.ru');  // Ещё один, если нужен
-    $mail->addAddress('musorov@gmail.com'); 
-    
+    $mail->addAddress('musorov@gmail.com');
+
     // Прикрепление файлов к письму необходимо реализовать на форме обратной связи блок для прикрепление файла
     // if (!empty($_FILES['myfile']['name'][0])) {
     //     for ($ct = 0; $ct < count($_FILES['myfile']['tmp_name']); $ct++) {
@@ -52,11 +52,11 @@ try {
     //         }
     //     }
     // }
-    
+
     // -----------------------
     // Само письмо
     // -----------------------
-    $mail->Subject = 'Сообщение с сайта Шелест';
+    $mail->Subject = 'Запрос с сайта Шелест';
     $mail->Body    = "<b>Имя:</b> $name <br>
         <b>Почта:</b> $email<br><br>
         <b>Сообщение:</b><br>$text";
@@ -64,8 +64,26 @@ try {
     // Проверяем отравленность сообщения
     if ($mail->send()) {
         echo "$msg";
+        //Чистим переменные перед отправкой подтверждения о доставке отравителю
+        $mail->ClearAddresses();
+        $mail->ClearAttachments();
+        $mail->IsHTML(false);
+        $mail->Subject = 'СК ССО Шелест';
+        $mail->Body    = "Привет, $name ! <br>
+        Вы оставили сообщение на сайте <a href='http://ск-шелест72.рф'><b>ск-шелест72.рф</b></a> <br>
+        Спасибо за проявленный интерес к нашей команде.<br>
+        В ближайшее время мы с Вами свяжемся по адресу электронной почты: <b>$email</b> <br><br>
+        С уважением, команда СК ССО Шелест.";
+        $mail->IsHTML(true);
+        $mail->AltBody = "Привет, $name !
+        Вы оставили сообщение на сайте ск-шелест72.рф
+        Спасибо за проявленный интерес к нашей команде.
+        В ближайшее время мы с Вами свяжемся по адресу электронной почты: $email
+        С уважением, команда СК ССО Шелест.";
+        $mail->addAddress($email);
+        $mail->send();
     } else {
-        echo "Сообщение не было отправлено. Неверно указаны настройки вашей почты";
+        echo "Сообщение не было отправлено. Проверьте адрес вашей почты";
         echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
 } catch (Exception $e) {
